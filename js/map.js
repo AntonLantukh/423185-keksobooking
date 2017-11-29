7bdd140742a79e5a11f5782d5000ca32e4ed630f
Anton Lantukh <lantukh@bk.ru>
2017-11-30 02:04:24 +0400

    Правки по замечаниям

--- js/map.js
+++ js/map.js
@@ -11,11 +11,7 @@ var noticeTemplate = document.querySelector('template').content.querySelector('.
 var noticeContainer = document.querySelector('.map');
 
 // Объявляем переменные
-var i = 0;
-var j = 1;
-var elementsList = [];
-var featuresArray = [];
-var offer = {
+var fakeOfferData = {
   'title': ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
   'address': '{{location.x}}, {{location.y}}',
   'price': range(1000, 1000000),
@@ -29,35 +25,32 @@ var offer = {
   'photos': []
 };
 
-
 // Убираем класс .map--faded
 mapDisplay.classList.remove('hidden');
 
 // Задаем цикл для функции генерации элемента (метки)
-for (i = 0; i < 8; i++) {
-  elementsList[i] = pinCreate(offer);
-  j++;
+for (var i = 0; i < 8; i++) {
+  var pinObject = pinCreate(i, fakeOfferData);
+  var pinNode = renderPin(pinObject);
+
+  fragment.appendChild(pinNode);
+
+  // Блок с данными о метке рендерим только для первого оффера
+  if (i === 0) {
+    var noticeNode = renderNotice(pinObject);
+    noticeContainer.appendChild(noticeNode);
+  }
 }
 
-// Отрисовка меток через вызов функции в цикле
-for (i = 0; i < elementsList.length; i++) {
-  fragment.appendChild(renderPin(elementsList[i]));
-}
 pinContainer.appendChild(fragment);
 
-// Отрисовка объявления через вызов функции в цикле
-for (i = 0; i < 1; i++) {
-  fragment.appendChild(renderNotice(elementsList[i]));
-}
-noticeContainer.appendChild(fragment);
-
-
 // Функция генерации элемента (метки)
-function pinCreate(info) {
-  var element = {
-    author: {'avatar': 'img/avatars/user0' + j + '.png'},
+function pinCreate(id, info) {
+  var sliceFrom = range(0, info.features.length - 2);
+  return {
+    author: {'avatar': 'img/avatars/user0' + (id + 1) + '.png'},
     offer: {
-      'title': info.title[i],
+      'title': shuffle(info.title).shift(),
       'address': (range(300, 900) - 40) + ', ' + (range(100, 500) - 40),
       'price': range(1000, 1000000),
       'type': info.type[range(0, 2)],
@@ -65,7 +58,7 @@ function pinCreate(info) {
       'guests': range(1, 8),
       'checkin': info.checkin[range(0, 2)],
       'checkout': info.checkout[range(0, 2)],
-      'features': shuffleArray(info.features, i, j),
+      'features': shuffle(info.features).slice(sliceFrom),
       'description': '',
       'photos': []
     },
@@ -74,7 +67,6 @@ function pinCreate(info) {
       'y': range(100, 500) - 40
     }
   };
-  return element;
 }
 
 // Функция отрисовки метки
@@ -82,6 +74,7 @@ function renderPin(list) {
   var pinElement = pinTemplate.cloneNode(true);
   pinElement.setAttribute('style', 'left: ' + list.location.x + 'px' + '; top: ' + list.location.y + 'px');
   pinElement.children[0].setAttribute('src', list.author.avatar);
+
   return pinElement;
 }
 
@@ -95,45 +88,53 @@ function defineFlatType(list) {
   } else if (list.offer.type === 'house') {
     type = 'Дом';
   }
+
   return type;
 }
 
 // Функция для создания списка удобств в рамках тега li
-function createFeatures(list) {
-  return '<li class="feature feature--' + list.offer.features[i] + '"></li>';
+function createFeatures(id, list) {
+
+  return '<li class="feature feature--' + list.offer.features[id] + '"></li>';
 }
 
 // Функция отрисовки объявления
 function renderNotice(list) {
+  var featuresArray = [];
   var noticeElement = noticeTemplate.cloneNode(true);
   noticeElement.children[2].textContent = list.offer.title;
   noticeElement.children[3].children[0].textContent = list.location.x + ', ' + list.location.y;
-  noticeElement.children[4].textContent = list.offer.price + '&#x20bd;/ночь';
+  noticeElement.children[4].innerHTML = list.offer.price + '&#x20bd;/ночь';
   noticeElement.children[5].textContent = defineFlatType(list);
   noticeElement.children[6].textContent = list.offer.rooms + ' для ' + list.offer.guests + ' гостей';
   noticeElement.children[7].textContent = 'Зазед после ' + list.offer.checkin + ', выезд до ' + list.offer.checkout;
   var featuresNotice = noticeElement.querySelector('.popup__features');
-  for (i = 0; i < list.offer.features.length; i++) {
-    featuresArray.push(createFeatures(list));
+  for (var j = 0; j < list.offer.features.length; j++) {
+    featuresArray.push(createFeatures(j, list));
   }
   featuresNotice.innerHTML = featuresArray.join(' ');
   noticeElement.children[9].textContent = list.offer.description;
   noticeElement.children[0].setAttribute('src', list.author.avatar);
-  return noticeElement;
-}
 
-// Функция для перемешивания массива и его обрезания
-function shuffleArray(array, l, m) {
-  for (l = array.length - 1; l > 0; l--) {
-    m = Math.floor(Math.random() * (l + 1));
-    var temp = array[l];
-    array[l] = array[m];
-    array[m] = temp;
-  }
-  return array.slice(range(0, 4));
+  return noticeElement;
 }
 
 // Функция для определения случайного числа в диапазоне
 function range(min, max) {
+
   return Math.round(Math.random() * (max - min) + min);
+}
+
+// Функция для перемешивания массива
+function shuffle(array) {
+  var counter = array.length;
+  while (counter > 0) {
+    var index = Math.floor(Math.random() * counter);
+    counter--;
+    var temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
 }
