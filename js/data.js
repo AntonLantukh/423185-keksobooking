8038938ee74ef3979914507bfb80e871ddad30e3
Anton Lantukh <lantukh@bk.ru>
2017-12-13 23:54:43 +0400

    Внедрение foreach, пересмотр схемы подгрузки данных

--- js/data.js
+++ js/data.js
@@ -7,9 +7,11 @@
     // Функция генерации объявления
     renderNotice: function (list) {
       var noticeTemplate = document.querySelector('template').content.querySelector('.map__card');
+      var noticeElement = noticeTemplate.cloneNode(true);
+      var featuresNotice = noticeElement.querySelector('.popup__features');
+      var imagesNotice = noticeElement.querySelector('.popup__pictures');
       var featuresArray = [];
       var imagesArray = [];
-      var noticeElement = noticeTemplate.cloneNode(true);
 
       noticeElement.children[2].textContent = list.offer.title;
       noticeElement.children[3].children[0].textContent = list.location.x + ', ' + list.location.y;
@@ -21,18 +23,16 @@
       noticeElement.children[0].setAttribute('src', list.author.avatar);
 
       // Заполняем разметку удобствами
-      var featuresNotice = noticeElement.querySelector('.popup__features');
-      for (var j = 0; j < list.offer.features.length; j++) {
-        featuresArray.push(createFeatures(j, list));
-      }
-      featuresNotice.innerHTML = featuresArray.join(' ');
+      list.offer.features.forEach(function (feature) {
+        featuresArray.push('<li class="feature feature--' + feature + '"></li>');
+        featuresNotice.innerHTML = featuresArray.join(' ');
+      });
 
       // Заполняем разметку фотографиями
-      var imagesNotice = noticeElement.querySelector('.popup__pictures');
-      for (j = 0; j < list.offer.photos.length; j++) {
-        imagesArray.push(createPhotos(j, list));
-      }
-      imagesNotice.innerHTML = imagesArray.join(' ');
+      list.offer.photos.forEach(function (image) {
+        imagesArray.push('<li><img src=' + image + ' width = "45" height="45"></li>');
+        imagesNotice.innerHTML = imagesArray.join(' ');
+      });
 
       return noticeElement;
     }
@@ -50,15 +50,4 @@
     }
     return type;
   }
-
-  // Функция для создания списка удобств в рамках тега li
-  function createFeatures(id, list) {
-    return '<li class="feature feature--' + list.offer.features[id] + '"></li>';
-  }
-
-  // Функция для создания списка фотографий в рамках тега li
-  function createPhotos(id, list) {
-    return '<li><img src=' + list.offer.photos[id] + ' width = "45" height="45"></li>';
-  }
-
 })();
