800325ed090d61c911819781fe8f3a694ca4b7e5
Anton Lantukh <lantukh@bk.ru>
2017-12-16 20:53:54 +0400

    Работа над функцией

--- js/filter.js
+++ js/filter.js
@@ -10,66 +10,69 @@
   var filterObject = {};
   var features = [];
 
-  // Событие клика на фильтр
+  // Событие клика на фильтр, создание динамиского объекта с фильтрами
   pinFilter.addEventListener('change', function () {
-    debugger;
     pinSelectors.forEach(function (array) {
       filterObject[array.name.substr(8)] = array.value;
-    })
+    });
     pinInputs.forEach(function (array) {
       if (array.checked) {
         features.push(array.value);
       }
-    })
+    });
     filterObject.features = features;
-    console.log(filterObject);
+    debugger;
+    filter();
   });
-    // pinNodes.push.event.target;
 
-  let filterPins = pinNodes.filter(function (pinNode) {
-      let isPassed = true
-      for (const key in filter) {
-          let isPassedKey
-          const pinOffer = pinNode.datashared.offer
-          const value = filter[key]
-          if (key === 'any' || (key === 'features' && filter[key].length === 0)) {
-              continue
-          }
-          if (key === 'price') {
-              isPassedKey = priceFilter(filter[key], pinOffer[key])
-          } else if (key === 'features') {
-              isPassedKey = featuresFilter(filter[key], pinOffer[key])
-          } else {
-              isPassedKey = pinNode.datashared.offer[key] === value
-          }
-          if (isPassedKey === false) {
-              isPassed = false
-              break
-          }
+  // Функця фильтрации пинов
+  function filter() {
+    var pinNodes = Array.from(pinContainer.children).slice(2);
+    var filterPins = pinNodes.filter(function (pinNode) {
+      var isPassed = true;
+      for (var key in pinNode.datashare.offer) {
+        var isPassedKey;
+        console.log(key);
+        var value = pinNode[key];
+        if (value === 'any' || (key === 'features' && filterObject[key].length === 0)) {
+          continue;
+        }
+        if (key === 'price') {
+          isPassedKey = priceFilter(filter[key], pinOffer[key])
+        } else if (key === 'features') {
+          isPassedKey = featuresFilter(filter[key], pinOffer[key])
+        } else {
+          isPassedKey = pinNode.datashared.offer[key] === value;
+        }
+        if (isPassedKey === false) {
+          isPassed = false;
+          break;
+        }
       }
-      return isPassed
-  })
+      return isPassed;
+    });
+  }
 
 
   pinNodes.forEach(function (pinNode) {
-      pinNode.hidden = filterPins.includes(pinNode)
-  })
+    pinNode.hidden = filterPins.includes(pinNode);
+  });
 
   function priceFilter (typePrice, offerPrice) {
-      if (typePrice === 'middle') {
-          return 10000 >= offerPrice && offerPrice <= 50000
-      } else if (typePrice === 'low') {
-          return offerPrice <= 10000
-      } else if (typePrice === 'high') {
-          return 50000 >= offerPrice
-      }
-      return true
+    if (typePrice === 'middle') {
+      return 10000 >= offerPrice && offerPrice <= 50000
+    } else if (typePrice === 'low') {
+      return offerPrice <= 10000
+    } else if (typePrice === 'high') {
+      return 50000 >= offerPrice
+    }
+    return true
   }
 
   function featuresFilter (filterFeatures, offerFeatures) {
-      const resultFeatures = filterFeatures.filter(function (filterFeature) {
-          return offerFeatures.includes(filterFeature)
-      })
-      return resultFeatures.length === filterFeatures.length
+    var resultFeatures = filterFeatures.filter(function (filterFeature) {
+      return offerFeatures.includes(filterFeature)
+    })
+    return resultFeatures.length === filterFeatures.length
   }
 })();
